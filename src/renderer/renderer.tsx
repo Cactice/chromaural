1466fe074da3d8a9dfe30f2c45d4bb2eb74c815b
Cactice <14835424+Cactice@users.noreply.github.com>
2020-06-21 02:08:36 +0900

    Move a lot of files

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Device } from 'node-hid';
+import {DeviceTable} from "./deviceTable/deviceTable.tsx"
 import style from "./style.styl"
 
 const userAgent = navigator.userAgent.toLowerCase();
@@ -10,68 +11,6 @@ const isElectron = userAgent.indexOf(' electron/') > -1;
 
 // Import the styles here to process them with webpack
 
-const Row = (device: Device) =>
-  Object.values(device).map((val: string, index: number) => (
-    <>
-      {(() => {
-        // eslint-disable-next-line no-nested-ternary
-        if (index === 2) {
-          return (
-            <td>
-              {val.split('/').map((substring) => (
-                <span className="device-table__span">
-                  {substring}
-                </span>
-              ))}
-            </td>
-          );
-        }
-        if (index === 5) {
-          return (
-            <td>
-              {val.split('/').map((substring) => {
-                if (substring === '') {
-                  return (
-                    <>
-                      <span className="is-warning is-light">
-                        Unknown device
-                      </span>
-                    </>
-                  );
-                }
-                return (
-                  <span className="is-info is-light">{substring}</span>
-                );
-              })}
-            </td>
-          );
-        }
-        return <td>{val}</td>;
-      })()}
-
-    </>
-  ));
-
-function DeviceTable(devices: Device[]) {
-  return (
-    <div className="app">
-      <table className="device-table">
-        <thead>
-          <tr>
-            {Object.keys(devices[0]).map((key) => (
-              <th>{key}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {devices.map((device) => (
-            <tr>{Row(device)}</tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
 
 function Example() {
   // Declare a new state variable, which we'll call "count"
