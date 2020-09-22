e0c30e44e4f17b2f7ab4d954cd5ddc82a0b80553
Cactice <14835424+Cactice@users.noreply.github.com>
2020-09-22 17:52:29 +0900

    Add webpack to visualizer

--- visualizer/src/background.ts
+++ visualizer/src/background.ts
@@ -1,9 +1,9 @@
 
 // A function to use as callback
-function callback(domContent) {
+function callback() {
   // nothing
 }
 
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.browserAction.onClicked.addListener(function (tab:any) {
   chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, callback);
 });
