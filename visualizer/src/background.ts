53cb23fb5a0292a0fe9ffe2285039f941795e2bc
Cactice <14835424+Cactice@users.noreply.github.com>
2020-10-11 16:53:31 +0900

    Delete visualizer

--- visualizer/src/background.ts
+++
@@ -1,9 +0,0 @@
-
-// A function to use as callback
-function callback() {
-  // nothing
-}
-
-chrome.browserAction.onClicked.addListener(function (tab:any) {
-  chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, callback);
-});
